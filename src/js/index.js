8fd57b735e654ac4e9c41798e5cbda072167eea3
Kalyel Mendes <kalyelmendesdapaixao@yahoo.com.br>
2026-02-01 22:58:24 -0300

    feat: adiciona a busca pelos dados do usuario ao apertar a tecla enter

--- src/js/index.js
+++ src/js/index.js
@@ -5,31 +5,34 @@ const inputSearch = document.getElementById('input-search');
 const btnSearch = document.getElementById('btn-search');
 const profileResults = document.querySelector('.profile-results');
 
-btnSearch.addEventListener('click', async () => {
+async function buscarUsuario() {
     const userName = inputSearch.value;
     if (!userName) {
         alert('Por favor, digite um nome de usuário do GitHub.');
         clearProfile(profileResults);
         return;
     }
-
     renderLoading(profileResults);
-
     try {
         const userData = await fetchGitHubUser(userName);
         const userRepos = await fetchGitHubUserRepos(userName);
-        
         if (!userData) {
             alert('Usuário não encontrado. Por favor, verifique o nome de usuário e tente novamente.');
             clearProfile(profileResults);
             return;
         }
-
         renderProfile(userData, userRepos, profileResults);
-
     } catch (error) {
         console.error('Erro ao buscar o perfil do usuário:', error);
         alert('Ocorreu um erro ao buscar o perfil do usuário. Por favor, tente novamente mais tarde.');
         clearProfile(profileResults);
+    }
+}
+
+btnSearch.addEventListener('click', buscarUsuario);
+
+inputSearch.addEventListener('keydown', function(e) {
+    if (e.key === 'Enter') {
+        buscarUsuario();
     }
 });
